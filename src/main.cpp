a632d0bbe97c71d9d511df249cb4567f772da951
ybeltagy <yousseffouadbeltagy@gmail.com>
2020-06-25 01:29:14 -0700

    removed everything

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
 
+int i;
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  i = 0;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  i++;
+  delay(1000);
+  Serial.println(i);
 }
